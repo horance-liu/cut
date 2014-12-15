4cb1531f57f5e1f9bce9c7aff85d72bd0af72569
horance <horance@outlook.com>
2014-12-15 19:43:06 +0800

    refactor description test

--- test/hamcrest/core/DescriptionTest.cpp
+++ test/hamcrest/core/DescriptionTest.cpp
@@ -6,109 +6,144 @@ USING_HAMCREST_NS
 
 struct DescriptionTest : testing::Test
 {
+    DescriptionTest& given_description()
+    {
+        return *this;
+    }
+
+    template <typename T>
+    DescriptionTest& when_append_value(const T& val)
+    {
+        desc.appendValue(val);
+        return *this;
+    }
+
+    void then_result_is(const std::string& result)
+    {
+        ASSERT_THAT(desc.to_s(), is(result));
+    }
+
+protected:
     Description desc;
 };
 
 TEST_F(DescriptionTest, bool_false)
 {
-    desc.appendValue(false);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(bool)false>")));
+    given_description()
+        .when_append_value(false)
+        .then_result_is("<(bool)false>");
 }
 
 TEST_F(DescriptionTest, bool_true)
 {
-    desc.appendValue(true);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(bool)true>")));
+    given_description()
+        .when_append_value(true)
+        .then_result_is("<(bool)true>");
 }
 
 TEST_F(DescriptionTest, null_integer)
 {
-    desc.appendValue((void*)0);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(void*)nil>")));
+    given_description()
+        .when_append_value((void*)0)
+        .then_result_is("<(void*)nil>");
 }
 
 TEST_F(DescriptionTest, NULL)
 {
-    desc.appendValue((void*)NULL);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(void*)nil>")));
+    given_description()
+        .when_append_value((void*)NULL)
+        .then_result_is("<(void*)nil>");
 }
 
 TEST_F(DescriptionTest, nullptr)
 {
-    desc.appendValue(nullptr);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(std::nullptr_t)nullptr>")));
+    given_description()
+        .when_append_value(nullptr)
+        .then_result_is("<(std::nullptr_t)nullptr>");
 }
 
 TEST_F(DescriptionTest, char)
 {
-    desc.appendValue((char)2);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(char)0x2/2>")));
+    given_description()
+        .when_append_value((char)2)
+        .then_result_is("<(char)0x2/2>");
 }
 
 TEST_F(DescriptionTest, signed_char)
 {
-    desc.appendValue((signed char)2);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(signed char)0x2/2>")));
+    given_description()
+        .when_append_value((signed char)2)
+        .then_result_is("<(signed char)0x2/2>");
 }
 
 TEST_F(DescriptionTest, unsigned_char)
 {
-    desc.appendValue((unsigned char)2);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(unsigned char)0x2/2>")));
+    given_description()
+        .when_append_value((unsigned char)2)
+        .then_result_is("<(unsigned char)0x2/2>");
 }
 
 TEST_F(DescriptionTest, short)
 {
-    desc.appendValue((short)2);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(short)0x2/2>")));
+    given_description()
+        .when_append_value((short)2)
+        .then_result_is("<(short)0x2/2>");
 }
 
 TEST_F(DescriptionTest, signed_short)
 {
-    desc.appendValue((signed short)2);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(short)0x2/2>")));
+    given_description()
+        .when_append_value((signed short)2)
+        .then_result_is("<(short)0x2/2>");
 }
 
 TEST_F(DescriptionTest, unsigned_short)
 {
-    desc.appendValue((unsigned short)2);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(unsigned short)0x2/2>")));
+    given_description()
+        .when_append_value((unsigned short)2)
+        .then_result_is("<(unsigned short)0x2/2>");
 }
 
 TEST_F(DescriptionTest, int)
 {
-    desc.appendValue(2);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(int)0x2/2>")));
+    given_description()
+        .when_append_value(2)
+        .then_result_is("<(int)0x2/2>");
 }
 
 TEST_F(DescriptionTest, unsigned_int)
 {
-    desc.appendValue(2U);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(unsigned int)0x2/2>")));
+    given_description()
+        .when_append_value(2U)
+        .then_result_is("<(unsigned int)0x2/2>");
 }
 
 TEST_F(DescriptionTest, long)
 {
-    desc.appendValue(2L);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(long)0x2/2>")));
+    given_description()
+        .when_append_value(2L)
+        .then_result_is("<(long)0x2/2>");
 }
 
 TEST_F(DescriptionTest, unsigned_long)
 {
-    desc.appendValue(2UL);
-    ASSERT_THAT(desc.to_s(), is(std::string("<(unsigned long)0x2/2>")));
+    given_description()
+        .when_append_value(2UL)
+        .then_result_is("<(unsigned long)0x2/2>");
 }
 
 TEST_F(DescriptionTest, c_style_string)
 {
-    desc.appendValue("hello, world");
-    ASSERT_THAT(desc.to_s(), is(std::string("<(char [13])\"hello, world\">")));
+    given_description()
+        .when_append_value("hello, world")
+        .then_result_is("<(char [13])\"hello, world\">");
 }
 
 TEST_F(DescriptionTest, std_string)
 {
-    desc.appendValue(std::string("hello, world"));
-    ASSERT_THAT(desc.to_s(), is(std::string("<(std::string)\"hello, world\">")));
+    given_description()
+        .when_append_value(std::string("hello, world"))
+        .then_result_is("<(std::string)\"hello, world\">");
 }
 
 TEST_F(DescriptionTest, append_text)
