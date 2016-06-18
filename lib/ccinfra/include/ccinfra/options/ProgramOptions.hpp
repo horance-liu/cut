75e4cf148a2954301e213a0e26ed07be0e4e855b
horance <horance@outlook.com>
2016-06-18 11:59:13 +0800

    Removed submodule ccinfra and hamcrest

--- lib/ccinfra/include/ccinfra/options/ProgramOptions.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef PROGRAMOPTIONS_Fri_Apr_10_18_34_44_2015_H
-#define PROGRAMOPTIONS_Fri_Apr_10_18_34_44_2015_H
-
-#include "program_options/OptionsDescription.hpp"
-#include "program_options/VariablesMap.hpp"
-
-#endif
--- lib/hamcrest/test/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "magellan/magellan.hpp"
-
-int main(int argc, char** argv)
-{
-    return magellan::run_all_tests(argc, argv);
-}
